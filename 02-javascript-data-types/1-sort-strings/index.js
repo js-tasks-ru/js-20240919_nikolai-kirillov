765448b44f1953f67c482cafef57636d99184ad8
CHOMGA <disangage@gmail.com>
2024-09-26 15:25:57 +0300

    Решение второго блока задач (исправления)

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -8,8 +8,8 @@ export function sortStrings(arr, param = "asc") {
     const locales = ["ru", "en"];
     const options = { sensitivity: "variant", caseFirst: "upper" };
     const collator = new Intl.Collator(locales, options);
-    ascended = (a, b) => collator.compare(a, b);
-    descended = (a, b) => collator.compare(b, a);
-    const result = param === "desc" ? arr.slice().sort(descended) : arr.slice().sort(ascended); 
+    const ascended = (a, b) => collator.compare(a, b);
+    const descended = (a, b) => collator.compare(b, a);
+    const result = param === "desc" ? arr.slice().sort(descended) : arr.slice().sort(ascended);
     return result;
 }
