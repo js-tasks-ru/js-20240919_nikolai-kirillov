2ca140d4dcb23e89ae0758ce48d73f99ed25ec61
CHOMGA <disangage@gmail.com>
2024-09-26 12:22:23 +0300

    Решение второго блока задач

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,11 +4,11 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
+export function sortStrings(arr, param = "asc") {
     const locales = ["ru", "en"];
     const options = { sensitivity: "variant", caseFirst: "upper" };
     const collator = new Intl.Collator(locales, options);
     ascended = (a, b) => collator.compare(a, b);
     descended = (a, b) => collator.compare(b, a);
-    return param === 'desc' ? arr.slice().sort(descended) : arr.slice().sort(ascended);
+    return param === "desc" ? arr.slice().sort(descended) : arr.slice().sort(ascended);
 }
