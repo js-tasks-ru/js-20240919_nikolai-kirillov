e92bdb4bdfd2a4f04c6ae3ae9d0eaa1c7de4452a
CHOMGA <disangage@gmail.com>
2024-09-26 11:40:45 +0300

    Решение второго блока задач

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,12 +5,10 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
-    const result = arr.toSorted(comparer);
-    return param === 'desc' ? result.reverse() : result
-}
-
-export function comparer(a, b) {
     const locales = ["ru", "en"];
     const options = { sensitivity: "variant", caseFirst: "upper" };
-    return a.localeCompare(b, locales, options);
+    const collator = new Intl.Collator(locales, options);
+    sortAsc = (a, b) => collator.compare(a, b);
+    const result = arr.toSorted(sortAsc);
+    return param === 'desc' ? result.reverse() : result;
 }
