c98f79eccfb5d6bafabaeec9bd0b37067fc91b3d
CHOMGA <disangage@gmail.com>
2024-10-09 22:10:45 +0300

    Решение пятого блока задач

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,47 @@
 export default class NotificationMessage {
+    static previousMessage;
 
+    constructor(message = '', options = {}) {
+        this.options = {...options};
+        this.message = message;
+        this.duration = options.duration;
+        this.type = options.type;
+        this.element = this.createNotificationElement();
+    }
+
+    createNotificationElement() {
+        const element = document.createElement('div');
+        element.innerHTML = this.createMessageTemplate();
+        if (this.type) element.firstElementChild.classList.add(this.type);
+        return element.firstElementChild;
+    }
+
+    createMessageTemplate() {
+        return `<div id='notificationID' class="notification" style="--value:${this.duration / 1000}s">
+                <div class="timer"></div>
+                <div class="inner-wrapper">
+                <div class="notification-header">${this.type}</div>
+                <div class="notification-body">
+                ${this.message}
+                </div>
+                </div>
+                </div>
+    `
+    }
+
+    show(container = document.body) {
+        if (NotificationMessage.previousMessage) NotificationMessage.previousMessage.remove();
+        NotificationMessage.previousMessage = this.element;
+        this.messageTimer = setTimeout(() => this.remove(), this.duration);
+        container.append(this.element);
+    }
+
+    remove() {
+        this.destroy();
+    }
+
+    destroy() {
+        if (this.element) this.element.remove();
+        if (this.messageTimer) clearTimeout(this.messageTimer);
+    }
 }
--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -1,6 +1,122 @@
 export default class SortableTable {
-  constructor(headerConfig = [], data = []) {
+  arrowElement = ``;
+  headers = [];
 
+  constructor(headerConfig = [], data = []) {
+    this.headerConfig = headerConfig;
+    this.data = data;
+    this.createHeaders();
+    this.createArrowElement();
+    this.element = this.createTalbeElement();
+    this.subElements = {
+      header: this.element.querySelector('[data-element="header"]'),
+      body: this.element.querySelector('[data-element="body"]'),
+    };
+  }
+
+  createHeaders() {
+    const headersFromConfig = [];
+    for (const header of this.headerConfig) {
+      headersFromConfig.push(header.id);
+    }
+    this.headers = headersFromConfig;
+  }
+
+  createArrowElement() {
+    const arrow = document.createElement('div');
+    arrow.innerHTML = `
+<span data-element="arrow" class="sortable-table__sort-arrow">
+  <span class="sort-arrow"></span>
+</span>`;
+    this.arrowElement = arrow;
+  }
+
+  createTalbeElement() {
+    const element = document.createElement('div');
+    element.innerHTML = this.createTableTemplate();
+    return element.firstElementChild;
+  }
+
+  createTableTemplate() {
+    return `
+<div data-element="productsContainer" class="products-list__container">
+  <div class="sortable-table">
+    <div data-element="header" class="sortable-table__header sortable-table__row">
+      ${this.createHeader()}
+    </div>
+    <div data-element="body" class="sortable-table__body">
+      ${this.createTable()}
+    </div>
+  </div>
+</div>`;
+  }
+
+  createHeader() {
+    let headerTemplate = ``;
+    this.headerConfig.forEach(header => {
+      headerTemplate += `
+<div class="sortable-table__cell" data-id="${header.id}" data-sortable="${header.sortable}">
+  <span>${header.title}</span>
+</div>`;
+    });
+    return headerTemplate;
+  }
+
+  createTable() {
+    let tableTemplate = ``;
+    this.data.forEach(cell => {
+      tableTemplate += `
+<a href="/products/3d-ochki-optoma-zf2300" class="sortable-table__row">
+  ${this.createTableByHeaders(cell)}
+</a>
+    `;
+    });
+    return tableTemplate
+  }
+
+  createTableByHeaders(cell) {
+    let cellTemplate = ``;
+    for (const header of this.headers) {
+      console.log(this.headers)
+      header === 'images'
+        ? cellTemplate += `<div class="sortable-table__cell"> <img class="sortable-table-image" alt="Image" src="${cell.images[0].url}"></div>`
+        : cellTemplate += `<div class="sortable-table__cell">${cell[header]}</div>`;
+
+    }
+    return cellTemplate;
+  }
+
+  sort(fieldValue, orderValue) {
+    this.data = this.sortValues(this.data, orderValue, fieldValue);
+    this.subElements.body.innerHTML = this.createTable();
+    const headerElement = document.querySelector(`[data-id=${fieldValue}]`);
+    headerElement.setAttribute('data-order', orderValue);
+    headerElement.appendChild(this.arrowElement);
+  }
+
+  sortValues(data, param = "asc", value) {
+    const field = this.headerConfig.find(el => el.id === value);
+    if (!field.sortable) return
+    if (field.sortType === 'string') {
+      const locales = ["ru", "en"];
+      const options = { sensitivity: "variant", caseFirst: "upper", numeric: true };
+      const collator = new Intl.Collator(locales, options);
+      const ascended = (a, b) => collator.compare(a[value], b[value]);
+      const descended = (a, b) => collator.compare(b[value], a[value]);
+      return param === "desc" ? data.slice().sort(descended) : data.slice().sort(ascended);
+    } else {
+      const ascended = (a, b) => (a[value] - b[value]);
+      const descended = (a, b) => (b[value] - a[value]);
+      return param === "desc" ? data.slice().sort(descended) : data.slice().sort(ascended);
+    }
+  }
+
+  remove() {
+    this.element.remove()
+  }
+
+  destroy() {
+    this.remove();
   }
 }
 
