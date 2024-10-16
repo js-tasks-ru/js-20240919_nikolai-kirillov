12e3833849d54b2f9763433f61a50b257d2345da
CHOMGA <disangage@gmail.com>
2024-10-16 23:09:44 +0300

    Решение шестого блока, Tooltip

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,6 +1,56 @@
 class Tooltip {
-  initialize () {
+  static instance;
+  element;
 
+  constructor() {
+    if (Tooltip.instance) {
+      return Tooltip.instance;
+    }
+    Tooltip.instance = this;
+  }
+
+  initialize() {
+    document.addEventListener('pointerover', this.handlePointerover);
+  }
+
+  handlePointerover = (event) => {
+    const closestElement = event.target.closest("div");
+    if (!closestElement) return;
+    if (!closestElement.dataset.tooltip) return;
+    document.addEventListener('pointerout', this.handlePointerout);
+    document.addEventListener('pointermove', this.handlePointermove);
+    this.render(closestElement.dataset.tooltip);
+  }
+
+  render(tooltip) {
+    const tooltipElement = document.createElement('div');
+    tooltipElement.innerHTML = `<div class="tooltip">${tooltip}</div>`;
+    this.element = tooltipElement.firstElementChild;
+    document.body.append(tooltipElement);
+  }
+
+  handlePointerout = (event) => {
+    document.removeEventListener('pointerout', this.handlePointerout);
+    document.removeEventListener('pointermove', this.handlePointermove);
+    this.remove();
+  }
+
+  handlePointermove = (event) => {
+    this.element.style.left = event.clientX + 8 + 'px';
+    this.element.style.top = event.clientY + 8 + 'px';
+  }
+
+  removeListeners() {
+    document.removeEventListener('pointerover', this.handlePointerover);
+  }
+
+  destroy() {
+    this.remove();
+    this.removeListeners();
+  }
+
+  remove() {
+    if (this.element) this.element.parentElement.remove();
   }
 }
 
